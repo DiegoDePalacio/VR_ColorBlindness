a5e966d0feab9885ae9ba8cbbf3f60f608411a9c
Gineton Alencar II <gineton2@gmail.com>
2017-09-24 19:02:57 -0700

    Game logic script started
    Now can keep track of ripe apples, collected apples, and the HUD.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,16 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameLogic : MonoBehaviour {
+public class GameLogic : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int ripeApples; //total ripe apples
+    private int collectedApples; //ripe apples collected
+
+    public Text signText;
+
+    private float timeLeft = 60.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        ripeApples = GameObject.FindGameObjectsWithTag("RipeApple").Length;
+        collectedApples = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //triggered on click:
+    //if apple is ripe, add to collectedApples and positive audio feedback
+    //else give negative audio feedback
+    public void OnAppleClick(GameObject other)
+    {
+        if (other.gameObject.CompareTag("RipeApple"))
+        {
+            collectedApples++;
+            UpdateHUD();
+        }
+    }
+
+    void UpdateHUD()
+    {
+        signText.text = "Ripe apples collected:" + collectedApples.ToString();
+
+        if (collectedApples == ripeApples)
+        {
+            signText.text = "You collected all the ripe apples!";
+        }
+    }
+
 }
